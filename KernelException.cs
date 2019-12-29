b8aa1e25fef1d63f02fa4de13f1d76c5bee1a59c
Mohammadreza Tarkhan <mohammadreza.tarkhan@gmail.com>
2019-12-29 23:01:51 +0330

    kernel exception public

--- KernelException.cs
+++ KernelException.cs
@@ -3,21 +3,13 @@ using System.Runtime.Serialization;
 
 namespace Sayda.Core.Domain
 {
-	internal sealed class KernelException : Exception
+	public sealed class KernelException : Exception
 	{
-		public KernelException ()
-		{
-		}
-
 		public KernelException (string message) : base(message)
 		{
 		}
 
 		public KernelException (string message, Exception innerException) : base(message, innerException)
-		{
-		}
-
-		protected KernelException (SerializationInfo info, StreamingContext context) : base(info, context)
 		{
 		}
 	}
