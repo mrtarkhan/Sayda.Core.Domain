613ccc9d81b088b88250913a7b1a7b0a7b4036dd
Mohammadreza Tarkhan <mohammadreza.tarkhan@gmail.com>
2019-12-29 22:34:07 +0330

    add guid entity/aggregate

---
+++ AggregateRootGuid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Sayda.Core.Domain
+{
+	public abstract class AggregateRoot : Entity
+	{
+		private readonly List<DomainEvent> _domainEvents = new List<DomainEvent>();
+		public virtual IReadOnlyList<DomainEvent> DomainEvents => _domainEvents;
+
+		protected virtual void AddEvent (DomainEvent newEvent)
+		{
+			_domainEvents.Add(newEvent);
+		}
+
+		public virtual void ClearEvents ()
+		{
+			_domainEvents.Clear();
+		}
+	}
+}
---
+++ EntityGuid.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Sayda.Core.Domain
+{
+	public abstract class Entity
+	{
+		public Guid Id { get; protected set; }
+
+		public override bool Equals (object obj)
+		{
+			var other = obj as Entity;
+
+			if (ReferenceEquals(other, null))
+				return false;
+
+			if (ReferenceEquals(this, other))
+				return true;
+
+			if (GetType() != other.GetType())
+				return false;
+
+			if (Id == Guid.Empty || other.Id == Guid.Empty)
+				return false;
+
+			return Id == other.Id;
+		}
+
+		public static bool operator == (Entity a, Entity b)
+		{
+			if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+				return true;
+
+			if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+				return false;
+
+			return a.Equals(b);
+		}
+
+		public static bool operator != (Entity a, Entity b)
+		{
+			return !(a == b);
+		}
+
+		public override int GetHashCode ()
+		{
+			return Id.GetHashCode();
+		}
+
+	}
+}
